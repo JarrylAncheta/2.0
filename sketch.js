110e41736223cd481dda3c6110a5193253de9419
JarrylAncheta <55153273+JarrylAncheta@users.noreply.github.com>
2021-10-19 22:00:56 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,58 +1,30 @@
-let titles = [
-"compliment",
-"conversation",
-"explore",
-"gas",
-"green",
-"podcast", 
-"rap", 
-"recipe",
-"restaurant",
-"stocks", 
-"tour",
-"walk",
-"water",
-"workout"]
 
-let task = [];
-let animating = false;
-let randomIndex;
-let button;
+// Get all the images
+image_array = [
+  'MyPics/Task_compliment.jpg',
+  'MyPics/Task_conversation.jpg',
+  'MyPics/Task_explore.jpg',
+  'MyPics/Task_gas.jpg',
+  'MyPics/Task_green.jpg',
+  'MyPics/Task_podcast.jpg',
+  'MyPics/Task_rap.jpg',
+  'MyPics/Task_recipe.jpg',
+  'MyPics/Task_restaurant.jpg',
+  'MyPics/Task_stocks.jpg',
+  'MyPics/Task_tour.jpg',
+  'MyPics/Task_walk.jpg',
+  'MyPics/Task_water.jpg',
+  'MyPics/Task_workout.jpg',
 
-function preload (){
-  for(let i = 0; i <= 11; i ++){  
-    task[i] = loadImage ("MyPics/Task_" + i + ".jpg")
-  }
-}
+]
 
-function setup() {
-  cnv= createCanvas (400,400);
-  background (200)
-  textSelection = ""
-  imageSelection = 0
-  console.log (task);
-  imageMode (CENTER)
-button = createButton ('gimme a random flick');
-   button.position(0, 0);
-  button.mousePressed(pick);
-  button.style("padding", "20px")
-  button.style ("background-color","38b07a");
- 
-}
+function get_random_image(){
+  // Get a random index
+  random_index = Math.floor(Math.random() * image_array.length);
 
-function draw() {  
-  background(200,0,0);
-  image(task[imageSelection], width/2, height/2, 200, 250);
-  text(textSelection, 200, 350)
-  fill (255)
-  textFont ('Avenir')
-  textStyle (BOLD)
-  textAlign (CENTER)
-  
-}
+  // Get an image at the random_index
+  selected_image = image_array[random_index]
 
-function pick() {
-  textSelection = (titles[int(random(titles.length))])
-  imageSelection = int(random(7))
-  print(imageSelection);
+  // Display the image
+  document.getElementById('image_shower').src = `./images/${selected_image}`
 }
