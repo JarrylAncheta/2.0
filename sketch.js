42b5e3aa613556d9987e84b0414bd62cc7fa538a
JarrylAncheta <55153273+JarrylAncheta@users.noreply.github.com>
2021-10-19 21:26:35 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,76 +1,57 @@
-let quotes = [{name: "compliment"}, {name: "conversation"},{name:"explore"}, {name: "gas"},{name: "green"},{name: "podcast"},{name: "rap"},{name: "recipe"},{name: "rap"},{name: "recipe"},{name: "restaurant"},{name: "stocks"},{name: "tour"},{name: "walk"},{name: "water"},{name: "workout"},]
+let titles = [
+"Dysfunctional Family.",
+"My mom's show",
+"Dad's show",
+"The Gen X parents' show.",
+"Out of british royalty drama",
+"80s nostalgia.", 
+" Drugs", 
+"For Tweenagers",
+"Depressed Actors",
+"Must watch.", 
+"Get your life together",
+"2000s theater geek",
+"Ran out of anime"]
 
-let randomIndex;
-let randompics = [];
-let imageCounter = 0;
+let task = [];
 let animating = false;
+let randomIndex;
 let button;
-function preload(){
 
-  for (let i = 0; i <=10; i++){
-
-randompics[i] = loadImage(`MyPics/Task_${i}.jpg`)
-
+function preload (){
+  for(let i = 0; i <= 11; i ++){  
+    task[i] = loadImage ("MyPics/Task_" + i + ".jpg")
   }
 }
-
-
 
 function setup() {
-  createCanvas(600, 600);
-  background(182,235,242);
-  textSize(32);
-  imageMode(CENTER);
-  frameRate(5);
-
-fill(255);
-text("click blue button to randomize",50,50);
-
-button = createButton("click for a helpful tip");
-button.mousePressed(buttonPressed);
-button.style("padding","20px");
-button.style("background-color","#49b7e3");
+  cnv= createCanvas (400,400);
+  background (200)
+  textSelection = ""
+  imageSelection = 0
+  console.log (tvshow);
+  imageMode (CENTER)
+button = createButton ('gimme a random flick');
+   button.position(0, 0);
+  button.mousePressed(pick);
+  button.style("padding", "20px")
+  button.style ("background-color","38b07a");
+ 
 }
 
-function draw() {
-
-if (animating == true){
-  clear();
-
-  image(randompics[imageCounter], width/2, height/2);
-
-  if (imageCounter < randompics.length-1)
-  imageCounter++;
-  console.log(imageCounter);
-} else{
-  imageCounter = 0;
+function draw() {  
+  background(200,0,0);
+  image(tvshow[imageSelection], width/2, height/2, 200, 250);
+  text(textSelection, 200, 350)
+  fill (255)
+  textFont ('Avenir')
+  textStyle (BOLD)
+  textAlign (CENTER)
+  
 }
 
-}
-
-function randomizer (){
-  animating = false;
-  if (quotes[0]){
-
-  // background(random(200,255));
-
-  randomIndex = int(random(quotes.length))
-  text(`${quotes[randomIndex].name}'s favorite color inspect;
-  ${quotes[randomIndex].color}`,50, 50);
-  image(random(randompics), width/2, height/2);
-  // text(dogs[randomIndex].name + "'s favorite color is" +
-  // dogs[randomIndex].color,50, 50);
-  quotes.splice(randomIndex, 1);
-  }
-  else {
-    background(random(226,225,85));
-    text("Have a great day :)",50,50);
-  }
-}
-
-function buttonPressed() {
-  animating = true;
-  setTimeout(randomizer,2500);
-
-
+function pick() {
+  textSelection = (titles[int(random(titles.length))])
+  imageSelection = int(random(7))
+  print(imageSelection);
 }
