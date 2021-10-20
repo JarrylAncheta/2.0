fdcc7c24928e8519fa2dd8e84ca86d0751c06f46
JarrylAncheta <55153273+JarrylAncheta@users.noreply.github.com>
2021-10-20 00:01:44 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,42 +1,80 @@
-let task = [];
-let animating = false;
-let randomIndex;
-let button;
+function setup() {
 
-function preload (){
-  for(let i = 0; i <= 11; i ++){  
-    task[i] = loadImage ("MyPics/Task_" + i + ".jpg")
+  createCanvas(1000, 1000);
+
+  background(color(random(255, 205), random(205, 255), random(205, 205)));
+  backgroundColor = color(random(255, 205), random(205, 255), random(205, 205));
+  textSize(30);
+  textFont(myFont);
+  fill(0);
+  frameRate(20);
+  imageMode(CENTER);
+  textAlign(CENTER);
+  text("Are you hungry,",width/2, height/2 -50);
+  text("and can't decide what to eat?",width/2, height/2);
+  text("click on the button below to explore",width/2, height/2 + 50);
+
+// create a button
+  button = createButton("Explore!");
+  button.position(width/2 -100, height/2+400);
+  button.size(200, 50);
+  button.style("font-size", "30px");
+  button.style('background-color', "#bfc8db");
+
+// if button is pressed
+  button.mousePressed(buttonPressed);
+}
+function draw() {
+  if (animate == true) {
+    clear();
+    fill(random(255, 205), random(205, 255), random(205, 205))
+    if (imageCounter < tasks.length - 1) {
+      imageCounter++;
+    } else {
+      imageCounter = 0;
+    }
+    Task = tasks[imageCounter]
+    image(Task.image, width / 2, height / 2);
   }
 }
 
-function setup() {
-  cnv= createCanvas (400,400);
-  background (200)
-  textSelection = ""
-  imageSelection = 0
-  console.log (task);
-  imageMode (CENTER)
-button = createButton ('gimme a random flick');
-   button.position(0, 0);
-  button.mousePressed(pick);
-  button.style("padding", "20px")
-  button.style ("background-color","38b07a");
- 
+function randomizer() {
+  animate = false;
+  if (tasks.length > 0) {
+    // get random object from stuffs
+    background(random(255, 205), random(205, 255), random(205, 205));
+    randomIndex = int(random(tasks.length));
+    fill(0);
+    Task = tasks[randomIndex]
+    image(Task.image, width / 2, height /2);
+    TaskName = task.name.replaceAll("_", " ");
+    text(
+      `How about some ${taskName} ?`,
+      width / 2,
+      height /1.2
+    );
+    // and then take that object out of the array
+    tasks.splice(randomIndex, 1);
+  } else {
+    //   draw the background again so it does not overlay the previous object
+    background(backgroundColor);    // when we show all food, nothing else to show
+    text("Seriously, you go all the way here?", width / 2, height / 2);
+    text("Make up your mind. Eat anything!",width/2, height/2 +50)
+  }
 }
 
-function draw() {  
-  background(200,0,0);
-  image(task[imageSelection], width/2, height/2, 200, 250);
-  text(textSelection, 200, 350)
-  fill (255)
-  textFont ('Avenir')
-  textStyle (BOLD)
-  textAlign (CENTER)
-  
+function buttonPressed() {
+  // if there's more than 1 food in the array, run the timeout
+  animate = Task.length > 1;
+  if (animate) {
+
+    setTimeout(randomizer, 1500);
+
+    fx.play();
+    // don't run the timeout + display the last food
+  } else {
+    randomizer()
+  }
 }
 
-function pick() {
-  textSelection = (titles[int(random(titles.length))])
-  imageSelection = int(random(7))
-  print(imageSelection);
-}
+
